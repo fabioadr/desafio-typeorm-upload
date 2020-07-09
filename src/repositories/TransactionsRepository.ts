1ec213bbb8a653de5edd823742a360b709f2e9b5
Fábio Diniz <fabioadr_jf@hotmail.com>
2020-07-09 19:30:26 -0300

    Serviços de criação e exclusão de transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,22 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    const transactions = await this.find();
+
+    const { income, outcome } = transactions.reduce<Balance>(
+      (balanceAcc: Balance, transaction) => {
+        balanceAcc[transaction.type] += transaction.value;
+
+        return balanceAcc;
+      },
+      balance,
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
+import { getCustomRepository, getRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const categoryDb = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let categoryId;
+    if (!categoryDb) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+      categoryId = newCategory.id;
+    } else {
+      categoryId = categoryDb.id;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryId,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
